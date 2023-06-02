a0104aa2c40a48e8b93c7c87407d4fc93a1d20cc
shazj23 <131261375+shazj23@users.noreply.github.com>
2023-06-02 13:58:08 +0100

    snowflake conn

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,8 +39,6 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # converts into a dataframe
 streamlit.dataframe(fruityvice_normalized)
-
-
-
+ 
 import snowflake.connector
 
