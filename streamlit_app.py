593d8765ea4859e7baccbc9c4158c0b20dae4266
shazj23 <131261375+shazj23@users.noreply.github.com>
2023-06-02 15:35:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ streamlit.stop()
 
 
 def get_fruit_load_list():
- with my_cur = my_cnx.cursor() as my_cur:
+ with my_cnx.cursor() as my_cur:
   my_cur.execute("select * from fruit_load_list")
   return my_cur.fetchall()
  
