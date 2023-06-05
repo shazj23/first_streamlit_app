dd69880998e7f926eb7bf6ca514995fafbaad898
shazj23 <131261375+shazj23@users.noreply.github.com>
2023-06-05 12:31:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,17 +44,7 @@ try:
   else:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
-
     
-
-
-
-
-
-
-
-
-
 streamlit.stop()
 
 #import snowflake.connector 
